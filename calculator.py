60881b12933a147991d49e281cb85cbba779f73e
JatinBhardwaj10 <128028349+JatinBhardwaj10@users.noreply.github.com>
2023-11-05 11:33:47 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a = int(input("Enter number 1:"))
+b =int(input("Enter number 2 :"))
+c =input("Enter Operation: +,-,*,/")
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+else:
+    print("Invalid data")
